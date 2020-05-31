2e5973e082f968c363fad1712cc404603932ab9e
idith almog <almog.idith@gmail.com>
2020-05-31 00:18:41 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,5 @@
 class User
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :knowledge
   def initailze (first_name,last_name)
     @first_name =first_name
     @last_name = last_name
