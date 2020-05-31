43c7daba19724d26a9d260779348890e526e3ee8
idith almog <almog.idith@gmail.com>
2020-05-31 00:05:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
+  def initailze(first_name,last_name)
+    @knowledge =[]
+  end
+  def learn(string)
+    @knowledge << string
+  end
+
 
 end
